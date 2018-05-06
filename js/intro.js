4e5511989e87c30548bc7261c0923d769376e20d
Albin Remnestål <albin.remnestal@gmail.com>
2018-05-06 23:44:05 +0200

    feat: fade in/out intro-chevron on scroll-past

--- js/intro.js
+++ js/intro.js
@@ -2,10 +2,18 @@ $(document).ready(function() {
 
     // scroll to legend on click
     $('#intro-chevron').click(function() {
-        console.log($('#intro').height())
         $('html, body').animate({
             scrollTop: $('#intro').height()
         }, 'slow')
+    })
+
+    // fadeIn/fadeOut effect on chevron based on scrolldistance
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > $('#intro').height()/2) {
+            $('#intro-chevron').fadeOut('3000')
+        } else {
+            $('#intro-chevron').fadeIn('3000')
+        }
     })
 
 })
