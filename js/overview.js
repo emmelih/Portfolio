77933ecc8a8517abad381aaacbc8765e4721945d
Albin Remnestål <albin.remnestal@gmail.com>
2018-05-06 21:32:38 +0200

    feat: add js for populating the project overview

--- js/overview.js
+++ js/overview.js
@@ -7,4 +7,39 @@ $(document).ready(function() {
         $('body').css('overflow-y', 'scroll')
     })
 
+    display_project(projects[0])
 })
+
+/* populate the overview-page with project information */
+function display_project (project) {
+    // add, date, title and project image
+    $('#overview-title').text(project.title)
+    $('#overview-date').text(project.date)
+    $('#overview-image').attr('src', project.image_url)
+    // add each paragraph of the description
+    project.description.forEach((par) => {
+        $('#overview-description').append(
+            $('<p/>', {
+                text: par
+            })
+        )
+    })
+    // add each paragraph of the statement of contribution
+    project.contribution.forEach((par) => {
+        $('#overview-contribution').append(
+            $('<p/>', {
+                text: par
+            })
+        )
+    })
+    // add each paragraph of the statement of contribution
+    project.team.forEach((member) => {
+        $('#overview-team').append(
+            $('<p/>', {
+                class: 'no-margin',
+                text: `• ${member}`
+            })
+        )
+    })
+
+}
