d7ac4e8ab4ecdefb8f2cb08749f00aefabcb7d86
emmelih <hanssonemmeli@gmail.com>
2018-05-07 22:54:45 +0200

    feat: add project information

--- data/projects.js
+++ data/projects.js
@@ -1,85 +1,123 @@
 var projects = [
     {
-        "title": "Bud - study companion",
-        "date": "January - April, 2018",
-        "image_url": "images/bud.jpg",
-        "summary": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        "description": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        'title': 'Bud\' - study companion',
+        'date': 'January - April, 2018',
+        'image_url': 'images/buddy.jpg',
+        'summary': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        'description': [
+            'This project was part of a course at KTH called Physical Interaction Design, where we were tasked to create a physical prototype of something, with a focus on the physical interaction itself. The theme of the prototype was "library" and to my team and I, we associate the library with studying. So we wanted to create something that would keep you company, as well as be useful. It took a few iterations to find out what we wanted our Bud\' to do, but that is all part of the process, and enabled us to think about the different materials and possible interactions.',
+            'We were really able get creative and create something hands-on. Inside Bud\', there is a Arduino UNO controlling the sensors, and the lights under the fluffy material. On Bud\'s head we created a stroke sensor with conductive thread.',
+            'Bud\' has a light sensor, sound sensor and temperature sensor, and, when stroked indicates the level of these three by lighting up different amounts of lamps on the LED strips on its sides, under the material. When in standby, the LEDs light up in a "breathing" pattern, which is supposed to be calming and nonpbtrusive. ' ,
+            'Heres a video we made of how Bud\' works!',
+            'https://drive.google.com/file/d/1TkU5cXudg43ez6O_Fb3opBjTouG7pbin/view?usp=sharing',
+            'And here are step by step instructions on how we made Bud\' ',
+            'https://docs.google.com/document/d/1yQfug_rYaPE4GElX3HiJwUWQt8E3_v_jzEgzQnraVHQ/edit?usp=sharing"'
         ],
-        "contribution": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        'contribution': [
+            'The team worked a lot together in creating Bud\'. We brainstormed together, to come with the  We Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         ],
-        "team": [
-            "Jane Doe",
-            "John Smith",
-            "Jimmy Hoffa"
+        'team': [
+            'Emmeli Hansson',
+            'Sofia Jacobsson',
+            'Henrik Johansson'
         ],
-        "tags": [
-            "Informationsdesign",
-            "Mjuk päls"
+        'tags': [
+            'Physical Interaction Design',
+            'Physical Prototyping',
+            'Fluffy'
         ],
-        "color": {
-            "main": "#ff627e",
-            "supplementary": "#fff2f2"
+        'color': {
+            'main': '#ff627e',
+            'supplementary': '#fff2f2'
         }
     },
     {
-        "title": "Kamikaze Clones",
-        "date": "January - April, 2018",
-        "image_url": "images/bud.jpg",
-        "summary": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "description": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        'title': 'Better Faulty than Sorry: Investigating Social Recovery Strategies to Minimize the Impact of Failure in Human-Robot Interaction',
+        'date': 'Spring 2017',
+        'image_url': 'images/bud.jpg',
+        'summary': 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ],
-        "contribution": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        'contribution': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         ],
-        "team": [
-            "Jane Doe",
-            "John Smith",
-            "Jimmy Hoffa"
+        'team': [
+            'Sara Engelhardt',
+            'Emmeli Hansson',
+            'Iolanda Leite'
         ],
-        "tags": [
-            "Game design"
+        'tags': [
+            'Robotics',
+            'CEUR Workshop Proceedings',
+            'Bachelor Thesis'
         ],
-        "color": {
-            "main": "#7d62ff",
-            "supplementary": "#f7f2ff"
+        'color': {
+            'main': '#7d62ff',
+            'supplementary': '#f7f2ff'
         }
     },
     {
-        "title": "Kex",
-        "date": "January - April, 2018",
-        "image_url": "images/bud.jpg",
-        "summary": "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        "description": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        'title': 'Global Goals: BTS',
+        'date': 'January - April, 2018',
+        'image_url': 'images/bud.jpg',
+        'summary': 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
         ],
-        "contribution": [
-            "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        'contribution': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
         ],
-        "team": [
-            "Jane Doe",
-            "John Smith",
-            "Jimmy Hoffa"
+        'team': [
+            'Jane Doe',
+            'John Smith',
+            'Jimmy Hoffa'
         ],
-        "tags": [
-            "Robotics",
-            "Thesis"
+        'tags': [
+            'Information Visualization',
+            'Global Goals'
         ],
-        "color": {
-            "main": "#9be67d",
-            "supplementary": "#f2fff2"
+        'color': {
+            'main': '#9be67d',
+            'supplementary': '#f2fff2'
+        }
+    },
+    {
+        'title': 'Kamiclones',
+        'date': 'October - December, 2017',
+        'image_url': 'images/bud.jpg',
+        'summary': 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'description': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        ],
+        'contribution': [
+            'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+        ],
+        'team': [
+            'Benjamin Délèze',
+            'Emmeli Hansson',
+            'Conrad Hildebrand',
+            'Daniel Isheden',
+            'Alexander Janson',
+            'Antonio Morais'
+        ],
+        'tags': [
+            'Game Design'
+        ],
+        'color': {
+            'main': '#9be67d',
+            'supplementary': '#f2fff2'
         }
     }
 ]
